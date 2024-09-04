c8b381f2ff8d9b9450036c5dc1c8518926280fa3
Shawn Hsiao <shawnhsiao@koodata.com.tw>
2024-09-04 20:16:06 +0800

    misc. add configs

--- src/config.py
+++ src/config.py
@@ -9,9 +9,15 @@ env_path = join(root_dir, ".env")
 load_dotenv(dotenv_path=env_path)
 
 
+# app related
+LOG_LEVEL = getenv("LOG_LEVEL", default="INFO")
+DEBUG = LOG_LEVEL == "DEBUG"
+
+# db related
 DB_HOST = getenv("DB_HOST", default="localhost")
 DB_USER = getenv("DB_USER", default="root")
 DB_PASSWORD = getenv("DB_PASSWORD", default="123456")
 DB_PORT = getenv("DB_PORT", default=3313)  # outer port
 DB_NAME = getenv("DB_NAME", default="gain_miles")
-DATABASE_URL = f"mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
+DATABASE_URL = f"mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"  # async
+SYNC_DATABASE_URL = DATABASE_URL.replace("aiomysql", "pymysql")

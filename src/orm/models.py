37963c8e545ce1aab17acec3795f078221e3632d
Shawn Hsiao <shawnhsiao@koodata.com.tw>
2024-09-04 19:39:03 +0800

    db. add models

---
+++ src/orm/models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from sqlmodel import SQLModel, Field, String
+
+
+class ProductSize(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True, index=True)  # autoincrement
+    name: str = Field(sa_type=String(100), nullable=False)  # S, M, L, XL
+    updated_at: datetime = Field(default_factory=datetime.utcnow, sa_column_kwargs={"onupdate": datetime.utcnow})
+
+
+class ProductColor(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True, index=True)  # autoincrement
+    name: str = Field(sa_type=String(120), nullable=False)
+    updated_at: datetime = Field(default_factory=datetime.utcnow, sa_column_kwargs={"onupdate": datetime.utcnow})
+
+
+class Product(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True, index=True)  # autoincrement
+    name: str = Field(sa_type=String(200), nullable=False)
+    code: str = Field(sa_type=String(100), nullable=False)
+    category: str = Field(sa_type=String(200), nullable=False)
+    unit_price: int
+    inventory: int
+    size_id: int = Field(foreign_key="product_size.id", nullable=False)
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow, sa_column_kwargs={"onupdate": datetime.utcnow})

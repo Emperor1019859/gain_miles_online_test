c1f4723cceb2f1be85b88ac68c95fbdd8d3bf35a
Shawn Hsiao <shawnhsiao@koodata.com.tw>
2024-09-04 19:53:13 +0800

    fix. models

--- src/orm/models.py
+++ src/orm/models.py
@@ -3,18 +3,24 @@ from sqlmodel import SQLModel, Field, String
 
 
 class ProductSize(SQLModel, table=True):
+    __tablename__ = "product_size"
+
     id: int = Field(default=None, primary_key=True, index=True)  # autoincrement
     name: str = Field(sa_type=String(100), nullable=False)  # S, M, L, XL
     updated_at: datetime = Field(default_factory=datetime.utcnow, sa_column_kwargs={"onupdate": datetime.utcnow})
 
 
 class ProductColor(SQLModel, table=True):
+    __tablename__ = "product_color"
+
     id: int = Field(default=None, primary_key=True, index=True)  # autoincrement
     name: str = Field(sa_type=String(120), nullable=False)
     updated_at: datetime = Field(default_factory=datetime.utcnow, sa_column_kwargs={"onupdate": datetime.utcnow})
 
 
 class Product(SQLModel, table=True):
+    __tablename__ = "product"
+
     id: int = Field(default=None, primary_key=True, index=True)  # autoincrement
     name: str = Field(sa_type=String(200), nullable=False)
     code: str = Field(sa_type=String(100), nullable=False)

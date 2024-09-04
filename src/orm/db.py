d306848d743cafd4f1375b65dd3c41fe6a5ea9ed
Shawn Hsiao <shawnhsiao@koodata.com.tw>
2024-09-04 20:20:58 +0800

    misc. add db tools

--- src/orm/db.py
+++ src/orm/db.py
@@ -1,4 +1,5 @@
 from sqlmodel import create_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from config import DATABASE_URL, SYNC_DATABASE_URL, DEBUG
@@ -6,3 +7,9 @@ from config import DATABASE_URL, SYNC_DATABASE_URL, DEBUG
 
 engine = create_async_engine(DATABASE_URL, echo=DEBUG, future=True)
 sync_engine = create_engine(SYNC_DATABASE_URL, echo=DEBUG, future=True)  # for sync query
+
+
+# for Dependency Injection
+async def get_session() -> AsyncSession:
+    async with AsyncSession(engine) as session:
+        yield session

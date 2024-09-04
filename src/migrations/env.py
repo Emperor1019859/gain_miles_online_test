eb52c08a42e14d9a5368dcb74fa083a08f0db12f
Shawn Hsiao <shawnhsiao@koodata.com.tw>
2024-09-04 19:48:02 +0800

    fix. migration issue

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -41,9 +41,8 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url,
+        url=DATABASE_URL,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -70,7 +69,7 @@ async def run_async_migrations() -> None:
     ini_config["sqlalchemy.url"] = DATABASE_URL
 
     connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        ini_config,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )

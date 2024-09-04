dbd9cafe15cec897c2eaa41a30b0f98f224358cf
Shawn Hsiao <shawnhsiao@koodata.com.tw>
2024-09-04 20:21:19 +0800

    feat. add endpoint `get_product`

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from uvicorn import run
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlmodel import select
+
+from orm.db import get_session
+from orm.models import Product
 
 
 app = FastAPI()
@@ -8,6 +12,14 @@ app = FastAPI()
 @app.get("/")
 async def root() -> str:
     return "Hello World"
+
+
+@app.get("/api/product/{id}")
+async def get_product(id: int, session=Depends(get_session)) -> Product | None:
+    query = select(Product).where(Product.id == id)
+    query_result = await session.exec(query)
+
+    return query_result.first()
 
 
 if __name__ == "__main__":
